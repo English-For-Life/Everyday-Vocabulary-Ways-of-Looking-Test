ba49d08a19bb3571395f85ee4014b059f2cb8601
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-21 12:40:02 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"That man does look rather strange but you shouldn’t ______ at him.",
+        options:["stare", "frown", "blink", "glare"],
+        answer: 0
+    },
+    {
+        q:"He made a hole in the fence so that he could ______ through without being seen.",
+        options:["wink", "peep", "blink", "peer"],
+        answer: 1
+    },
+    {
+        q:"If you go out into bright sunlight after being in the dark, you sometimes ______ .",
+        options:["peep", "glare", "blink", "peer"],
+        answer: 2
+    },
+    {
+        q:"Children often stand outside the bicycle shop and ______ at the wonderful machines in the window.",
+        options:["frown", "wink", "glare", "gaze"],
+        answer: 3
+    },
+    {
+        q:"We ______ if we are rather annoyed or if we are concentrating.",
+        options:["frown", "stare", "gaze", "peer"],
+        answer: 0
+    },
+    {
+        q:"Did you ______ someone pass the window a moment ago? - I thought I just saw someone.",
+        options:["wink", "glimpse", "glare", "frown"],
+        answer: 1
+    },
+    {
+        q:"I thought he was serious until I saw him ______ at me to show he was joking.",
+        options:["frown", "glance", "wink", "glimpse"],
+        answer: 2
+    },
+    {
+        q:"Grandfather has very bad eyes. He has to ______ at the newspaper to read it.",
+        options:["peep", "wink", "blink", "peer"],
+        answer: 3
+    },
+    {
+        q:"I saw the motorist get out of his car and ______ furiously at the other driver who had run into the back of him.",
+        options:["glare", "gaze", "blink", "wink"],
+        answer: 0
+    },
+    {
+        q:"I saw him ______ quickly at his watch.",
+        options:["stare", "glance", "gaze", "wink"],
+        answer: 1
+    }
+
+
+
+]
